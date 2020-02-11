3cf0c572e7f192f9a5aa0f7d8f8d275e7f7bd8b6
Ramon Sanchez <songwright66@gmail.com>
2020-02-11 11:17:15 -0800

    Imported body-parser

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 import express from 'express'; // This is the original line from the tutorial.
 // const express = require("express"); // This is the usual way to bring in express.
+import bodyParser from 'body-parser';
+
 const app = express();
 
+app.use(bodyParser.json());
+
 app.get('/hello', (req, res) => res.send('Hello!'));
-app.post('/hello', (req, res) => res.send('Hello post!'));
+app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}!`));
 
 app.listen(8000, () => console.log('Listening on port 8000'));
