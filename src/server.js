925efc982ec7304ae9044a5aa1044134152e629c
Ramon Sanchez <songwright66@gmail.com>
2020-02-11 10:53:05 -0800

    Set up express in server.js with test message

--- src/server.js
+++ src/server.js
@@ -1,3 +1,8 @@
-import express from 'express';
-
+// import express from 'express'; This is the bad line from the tutorial.
+const express = require("express");
 const app = express();
+
+app.get('/hello', (req, res) => res.send('Hello!'));
+
+app.listen(8000, () => console.log('Listening on port 8000'));
+
