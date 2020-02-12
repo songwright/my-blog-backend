19d03595cfbcf7f45b79d59b3102d060601893d1
Ramon Sanchez <songwright66@gmail.com>
2020-02-12 10:21:12 -0800

    Refactored upvote function in server.js

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-const withDB = async (operations) => {
+const withDB = async (operations, res) => {
   // Perform database functions
   try {
     // Connect to MongoDB client
@@ -27,37 +27,29 @@ const withDB = async (operations) => {
 
 app.get('/api/articles/:name', async (req, res) => {
   withDB(async (db) => {
-      const articleName = req.params.name;
+    const articleName = req.params.name;
 
-      const articleInfo = await db.collection('articles').findOne({ name: articleName });
-      res.status(200).json(articleInfo);
-  });
+    const articleInfo = await db.collection('articles').findOne({ name: articleName });
+    res.status(200).json(articleInfo);
+  }, res);
 });
 
 app.post('/api/articles/:name/upvote', async (req, res) => {
-  try {
-
-      const articleName = req.params.name;
-      
-      const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
-      const db = client.db('my-blog');
-      
-      // Database query
-      const articleInfo = await db.collection('articles').findOne({ name: articleName});
-      await db.collection('articles').updateOne({ name: articleName}, {
-        // MongoDB syntax for incrementing the upvote property
-        '$set': {
-          upvotes: articleInfo.upvotes +1,
-        }
-      });
-      const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName});
-      
-      res.status(200).json(updatedArticleInfo);
-      
-      client.close();
-  } catch (error) {
-    res.status(500).json({ message: 'Error connecting to database', error});
-  }
+  withDB(async (db) => {
+    const articleName = req.params.name;
+    
+    // Database query
+    const articleInfo = await db.collection('articles').findOne({ name: articleName});
+    await db.collection('articles').updateOne({ name: articleName}, {
+      // MongoDB syntax for incrementing the upvote property
+      '$set': {
+        upvotes: articleInfo.upvotes +1,
+      }
+    });
+    const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName});
+    
+    res.status(200).json(updatedArticleInfo);
+  }, res)
 });
 
 app.post('/api/articles/:name/add-comment', (req, res) => {
