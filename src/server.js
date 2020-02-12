61908f8c3fce358bd66eb69cc9ba7535add26615
Ramon Sanchez <songwright66@gmail.com>
2020-02-11 21:46:29 -0800

    Created mongodb get function in server.js

--- src/server.js
+++ src/server.js
@@ -1,25 +1,28 @@
 import express from 'express'; // This is the original line from the tutorial.
 // const express = require("express"); // This is the usual way to bring in express.
 import bodyParser from 'body-parser';
-
-const articlesInfo = {
-  'learn-react': {
-    upvotes: 0,
-    comments: []
-  },
-  'learn-node': {
-    upvotes: 0,
-    comments: []
-  },
-  'my-thoughts-on-resumes': {
-    upvotes: 0,
-    comments: []
-  }
-}
+import { MongoClient } from 'mongodb';
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.get('/api/articles/:name', async (req, res) => {
+  try {
+      const articleName = req.params.name;
+
+      const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+      const db = client.db('my-blog');
+
+      const articleInfo = await db.collection('articles').findOne({ name: articleName });
+      res.status(200).json(articleInfo);
+
+      client.close();
+  } catch (error) {
+    res.status(500).json({ message: 'Error connecting to database', error});
+  }
+
+})
 
 app.post('/api/articles/:name/upvote', (req, res) => {
   const articleName = req.params.name;
@@ -41,3 +44,18 @@ app.listen(8000, () => console.log('Listening on port 8000'));
 // app.get('/hello', (req, res) => res.send('Hello!'));
 // app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}`));
 // app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}!`));
+
+// const articlesInfo = {
+//   'learn-react': {
+//     upvotes: 0,
+//     comments: []
+//   },
+//   'learn-node': {
+//     upvotes: 0,
+//     comments: []
+//   },
+//   'my-thoughts-on-resumes': {
+//     upvotes: 0,
+//     comments: []
+//   }
+// }
