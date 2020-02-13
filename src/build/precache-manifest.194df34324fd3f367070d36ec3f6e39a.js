21c6f18176e36798c2d8430d7153cb398d5d39f3
Ramon Sanchez <songwright66@gmail.com>
2020-02-13 09:17:55 -0800

    Add build files

---
+++ src/build/precache-manifest.194df34324fd3f367070d36ec3f6e39a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8888b70fb0a3b4f5bbf7e07a5ed1db6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "254ab9d60df9403d5f8a",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "5d552149062f5f1eb2af",
+    "url": "/static/js/2.147e4ddc.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.147e4ddc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "254ab9d60df9403d5f8a",
+    "url": "/static/js/main.fd0c643b.chunk.js"
+  },
+  {
+    "revision": "1a6842cff40cdbd7ee88",
+    "url": "/static/js/runtime-main.d7f357ff.js"
+  }
+]);
